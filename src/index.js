deb5915175731c5aefd1822c45de600898bb5a03
jaq-h <jacques.henri.hebert@gmail.com>
2020-06-15 15:40:17 -0500

    vercel test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  withRouter,
+  BrowserRouter as Router
 } from 'react-router-dom'
 
 const MyApp = () => (
